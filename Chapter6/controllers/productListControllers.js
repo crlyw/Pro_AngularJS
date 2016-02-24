3a19e4d352c37e1985ea85984287ae3bce49fa02
YAWEI LI <jsyclyw@gmail.com>
2016-02-24 16:43:31 -0500

    save changes and update to chapter 9

--- Chapter6/controllers/productListControllers.js
+++ Chapter6/controllers/productListControllers.js
@@ -1,7 +1,7 @@
 angular.module("sportsStore")
     .constant("productListActiveClass","btn-primary") //custom constant definition
     .constant("productListPageCount", 3)//when defined as a constant, it should be injected
-    .controller("productListCtrl", function($scope,$filter,productListActiveClass,productListPageCount){
+    .controller("productListCtrl", function($scope,$filter,productListActiveClass,productListPageCount, cart){
     
     var selectedCategory = null;
     
@@ -28,5 +28,9 @@ angular.module("sportsStore")
     
     $scope.getPageClass = function(page){
         return $scope.selectedPage == page? productListActiveClass : "";
+    }
+    
+    $scope.addProductToCart = function(product){
+        cart.addProduct(product.id, product.name, product.price);
     }
 });
